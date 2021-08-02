8504075a1ba66a3a28f6d7453e0ee612d1659ee8
pratyushashukla <pratyusha.shukla212@gmail.com>
2021-08-02 14:46:44 +0530

    Microservice Producer added

---
+++ SimpleKafkaProject/SimpleKafkaProject/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Confluent.Kafka;
+using System.Net;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Kafka.Public;
+using Kafka.Public.Loggers;
+using System.Text;
+
+namespace SimpleKafkaProject
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            //try
+            //{
+                Builder(args).Build().Run();
+             //   throw new NotImplementedException();
+           // }
+            //catch(NotImplementedException e)
+            //{
+            //    e.GetHashCode;
+            //}
+            
+        }
+        private static IHostBuilder Builder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, collection) =>
+            {
+                collection.AddHostedService<KafkaConsumerHostedService>();
+                collection.AddHostedService<KafkaProducerHostedService>();
+                
+            });
+    }
+    public class KafkaConsumerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaConsumerHostedService> _logger;
+        private readonly ClusterClient _cluster;
+        public KafkaConsumerHostedService(ILogger<KafkaConsumerHostedService> logger)
+        {
+            _logger = logger;
+            _cluster = new ClusterClient(new Kafka.Public.Configuration
+            {
+                Seeds = "localhost:9092"
+
+            }, new ConsoleLogger());
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _cluster.ConsumeFromLatest("demo");
+            _cluster.MessageReceived += record =>
+            {
+                _logger.LogInformation($"Recived: {Encoding.UTF8.GetString(record.Value as byte[])}");
+            };
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _cluster?.Dispose();
+            return Task.CompletedTask;
+
+        }
+    }
+    public class KafkaProducerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaProducerHostedService> _logger;
+        private readonly IProducer<Null, string> _producer;
+        public KafkaProducerHostedService(ILogger<KafkaProducerHostedService> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9092"
+            };
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                var value = $"Hii P {i}";
+                _logger.LogInformation(value);
+                await _producer.ProduceAsync("demo", new Message<Null, string>()
+                {
+                    Value = value
+                },cancellationToken);
+            }
+            _producer.Flush(TimeSpan.FromSeconds(10));
+            
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _producer?.Dispose();
+            return Task.CompletedTask;
+            
+        }
+    } 
+}
